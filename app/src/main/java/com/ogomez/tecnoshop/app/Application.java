09a22215b1a78752ec26cc9cdc1a81f99085e6be
Ing Oswaldo Gomez <oswaldo.gh89@gmail.com>
2015-09-04 16:59:52 -0500

    Se agrega nuevo icono de chat

---
+++ app/src/main/java/com/ogomez/tecnoshop/app/Application.java
@@ -0,0 +1,12 @@
+package com.ogomez.tecnoshop.app;
+
+
+/**
+ * Created by Oswaldo Gomez on 03/09/2015.
+ */
+public class Application extends android.app.Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
